d111fbd8da5b9237cc1d651b53eaf9a2ac1cfc00
yellowgram1543 <anushaanand1543@gmail.com>
2024-06-30 12:26:09 +0530

    written code for loop_pattern_1 and also corrected it

--- src/Loop_patterns_1.java
+++ src/Loop_patterns_1.java
@@ -1,5 +1,21 @@
 public class Loop_patterns_1{
     public static void main(String[] args) {
+/*
+print the following pattern:
+*
+* *
+* * *
+* * * *
+* * * * *
 
+*/
+        int i;
+        int j;
+        for (i=0; i<6;i++){
+            for (j=0; j<i;j++){
+                System.out.print("* ");
+            }
+            System.out.println( );
+        }
     }
 }
