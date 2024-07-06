a74112a8745237e53c5a2f7410bf370dc3e6a6fb
yellowgram1543 <anushaanand1543@gmail.com>
2024-07-06 21:33:16 +0530

    written code for multiplicationTable and sumOfOddNumbers

---
+++ src/multiplicationTable.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class multiplicationTable {
+    public static void main(String[] args) {
+     Scanner scanner = new Scanner(System.in);
+        System.out.println(" Enter a number for which you want to generate the multiplication table");
+        int number = scanner.nextInt();
+        printMultiplication(number);
+    }
+    public static void printMultiplication(int number){
+        int i = 1;
+        while(i<=10){
+            int result = multiply(number,i);
+            System.out.println(number + " x " + i +" = " + result );
+            i++;
+
+        }
+    }
+    public static int multiply(int a, int b){
+        return a*b;
+    }
+}
---
+++ src/sumOfOddNumbers.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class sumOfOddNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the last number till where you want the sum: ");
+        int num = scanner.nextInt();
+        oddnum(num);
+        System.out.println("\b" + " = " + oddNumbers(num) );
+
+    }
+    public static int oddNumbers(int num){
+        int i = 1;
+        int sum = 0;
+        while(num>=i){
+            sum = sum + i;
+            i = i + 2;
+        }
+        return sum;
+    }
+    public static void oddnum(int num){
+        int i = 1;
+        while(i<=num){
+            System.out.print(i+ " +");
+            i+=2;
+        }
+    }
+}
