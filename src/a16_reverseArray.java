8fb88f97010d8b5b2982bd1e90a7a90062345f77
yellowgram1543 <anushaanand1543@gmail.com>
2024-07-24 20:47:46 +0530

    changed code

--- src/a16_reverseArray.java
+++ src/a16_reverseArray.java
@@ -4,22 +4,28 @@ import java.util.Scanner;
 public class a16_reverseArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Size input: ");
+        System.out.print("Enter array size: ");
         int size = scanner.nextInt();
         int[] array = new int[size];
+        System.out.println("Enter array elements:");
         for (int i = 0; i < size; i++) {
             array[i] = scanner.nextInt();
         }
         System.out.println("Original array: " + Arrays.toString(array));
-        newArray(array);
+        reverseArray(array);
     }
 
-    public static void newArray(int[] array){
-        int[] newarray = new int[array.length];
-        for (int i = 0; i < array.length; i++) {
-            newarray[i] = array[array.length - i - 1];
+    public static void reverseArray(int[] array) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+            left++;
+            right--;
         }
-        System.out.println("Reversed array: " + Arrays.toString(newarray));
+        System.out.println("Reversed array: " + Arrays.toString(array));
     }
 }
 
