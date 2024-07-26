8c3964b27dccde229fe484f7d711526281a47fdd
yellowgram1543 <anushaanand1543@gmail.com>
2024-07-26 20:51:58 +0530

    changed code

--- src/a15_reverseArray.java
+++ src/a15_reverseArray.java
@@ -4,21 +4,20 @@ import java.util.Scanner;
 public class a15_reverseArray {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter array size: ");
+        System.out.println("Enter the size of array: ");
         int size = scanner.nextInt();
         int[] array = new int[size];
-        System.out.println("Enter array elements:");
-        for (int i = 0; i < size; i++) {
+        System.out.println("Enter array elements: ");
+        for (int i = 0; i < size ; i++){
             array[i] = scanner.nextInt();
         }
         System.out.println("Original array: " + Arrays.toString(array));
-        reverseArray(array);
+        reverse(array);
     }
-
-    public static void reverseArray(int[] array) {
+    public static void reverse(int[]array){
         int left = 0;
         int right = array.length - 1;
-        while (left < right) {
+        while (left < right ){
             int temp = array[left];
             array[left] = array[right];
             array[right] = temp;
@@ -28,7 +27,3 @@ public class a15_reverseArray {
         System.out.println("Reversed array: " + Arrays.toString(array));
     }
 }
-
-
-
-
